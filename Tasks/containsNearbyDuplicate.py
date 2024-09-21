6c715019b6a872af9c7ae0dd2b95cb7c620f438e
Guruprasad Subhash Tandlekar <67339327+guruu27@users.noreply.github.com>
2024-09-21 01:00:00 -0400

    Create containsNearbyDuplicate.py

---
+++ Tasks/containsNearbyDuplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if num in hashmap and abs(i-hashmap[num])<=k:
+                return True
+            hashmap[num]=i
+        return False
